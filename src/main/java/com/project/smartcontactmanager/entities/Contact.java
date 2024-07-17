68e2ede5cab013d6e4799c3ff5cd799c0e4776e4
Yash Gupta <yash45885@gmail.com>
2024-07-17 19:25:27 +0530

    Correct duplicate issue

--- src/main/java/com/project/smartcontactmanager/entities/Contact.java
+++ src/main/java/com/project/smartcontactmanager/entities/Contact.java
@@ -10,102 +10,104 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "CONTACT")
+@Table(name = "CONTACT", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"email", "contact_number", "user_id"})
+})
 public class Contact {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int contact_id;
-	private String contact_name;
-	@Column(nullable = false)
-	private String contact_number;
-	@Column(unique = true)
-	private String email;
-	private String nickname;
-	private String contact_designation;
-	private String image;
-	@Column(length = 50000)
-	private String description;
-	
-	public Contact() {
-		super();
-	}
-	
-	@ManyToOne(cascade = CascadeType.ALL)
-	@JsonIgnore //To avoid circular dependency and rest other enteries will be serialize
-	private User user;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int contact_id;
+    private String contact_name;
+    @Column(nullable = false)
+    private String contact_number;
+    private String email;
+    private String nickname;
+    private String contact_designation;
+    private String image;
+    @Column(length = 50000)
+    private String description;
+    
+    public Contact() {
+        super();
+    }
+    
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JsonIgnore // To avoid circular dependency and rest other entries will be serialized
+    private User user;
 
-	public int getContact_id() {
-		return contact_id;
-	}
+    public int getContact_id() {
+        return contact_id;
+    }
 
-	public void setContact_id(int contact_id) {
-		this.contact_id = contact_id;
-	}
+    public void setContact_id(int contact_id) {
+        this.contact_id = contact_id;
+    }
 
-	public String getContact_name() {
-		return contact_name;
-	}
+    public String getContact_name() {
+        return contact_name;
+    }
 
-	public void setContact_name(String contact_name) {
-		this.contact_name = contact_name;
-	}
+    public void setContact_name(String contact_name) {
+        this.contact_name = contact_name;
+    }
 
-	public String getContact_number() {
-		return contact_number;
-	}
+    public String getContact_number() {
+        return contact_number;
+    }
 
-	public void setContact_number(String contact_number) {
-		this.contact_number = contact_number;
-	}
+    public void setContact_number(String contact_number) {
+        this.contact_number = contact_number;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public String getNickname() {
-		return nickname;
-	}
+    public String getNickname() {
+        return nickname;
+    }
 
-	public void setNickname(String nickname) {
-		this.nickname = nickname;
-	}
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-	public String getContact_designation() {
-		return contact_designation;
-	}
+    public String getContact_designation() {
+        return contact_designation;
+    }
 
-	public void setContact_designation(String contact_designation) {
-		this.contact_designation = contact_designation;
-	}
+    public void setContact_designation(String contact_designation) {
+        this.contact_designation = contact_designation;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getImage() {
+        return image;
+    }
 
-	public void setImage(String image) {
-		this.image = image;
-	}
+    public void setImage(String image) {
+        this.image = image;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public User getUser() {
-		return user;
-	}
+    public User getUser() {
+        return user;
+    }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
